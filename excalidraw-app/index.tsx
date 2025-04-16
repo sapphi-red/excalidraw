ae5995d0f632e64ac9cc2bcc638c6e052b0714fa
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-04-16 12:53:04 +0900

    chore: remove some plugins

--- excalidraw-app/index.tsx
+++ excalidraw-app/index.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { registerSW } from "virtual:pwa-register";
 
 import "../excalidraw-app/sentry";
 
@@ -9,7 +8,6 @@ import ExcalidrawApp from "./App";
 window.__EXCALIDRAW_SHA__ = import.meta.env.VITE_APP_GIT_SHA;
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
-registerSW();
 root.render(
   <StrictMode>
     <ExcalidrawApp />
